17c2d682397ee27dc229eb32178b7d79af5f7b9c
jasasuster <92645266+jasasuster@users.noreply.github.com>
2023-03-06 19:13:58 +0100

    Added reading from command line

--- RoparjiZlatihPalic/RoparjiZlatihPalic.cpp
+++ RoparjiZlatihPalic/RoparjiZlatihPalic.cpp
@@ -1,31 +1,17 @@
-﻿// RoparjiZlatihPalic.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+    if (argc != 2) return -1;
 
-    std::ifstream infile("./tests/testna_datoteka1.txt");
+    std::ifstream infile(argv[1]);  // open file to read from
 
     int G;
 
-    if (infile.is_open()) {   //checking whether the file is open
-        while (infile >> G) {
+    if (infile.is_open()) {   // checking whether the file is open
+        while (infile >> G) {   // read line by line
             std::cout << G << "\n";
         }
     }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
